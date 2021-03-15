f508d8259f00a2cbaf117a6a235f98872d4649aa
LiWeNHuI <sdgrlwh@163.com>
2021-03-15 17:17:06 +0800

    通过https://github.com/vance-liu/flutter_easyrefresh/tree/null_safety获取代码，为本地使用

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/liwenhui/work/flutter_sdk/flutter_2.x.x"
+export "FLUTTER_APPLICATION_PATH=/Users/liwenhui/work/project/flutter/demo/flutter_easyrefresh-null_safety/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=2.1.8"
+export "FLUTTER_BUILD_NUMBER=42"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
